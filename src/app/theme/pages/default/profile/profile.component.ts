a23f932ee75beed25ea70df0492fd3004795a930
Abdul Wahab Shah <wahab.shah23@gmail.com>
2018-06-26 13:01:41 +0500

    Profile Changes
    Profile Changes and file modifications

--- src/app/theme/pages/default/profile/profile.component.ts
+++ src/app/theme/pages/default/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Helpers } from '../../../../helpers';
+import { ScriptLoaderService } from '../../../../_services/script-loader.service';
 
 
 @Component({
     selector: "app-profile",
     templateUrl: "./profile.component.html",
+    styleUrls: ["./profile.component.css"],
     encapsulation: ViewEncapsulation.None,
 })
 export class ProfileComponent implements OnInit {
@@ -12,14 +14,35 @@ export class ProfileComponent implements OnInit {
     profileSectionHidden:boolean
     securitySectionHidden:boolean
     paymentSectionHidden:boolean
+    currencyDDLHidden:boolean
+    
 
-    constructor() {
+    constructor(private _script: ScriptLoaderService) {
 
     }
     ngOnInit() {
         this.profileSectionHidden=false;
         this.securitySectionHidden=true;
         this.paymentSectionHidden=true;
+        this.currencyDDLHidden=true;
+    }
+
+    ngAfterViewInit() {
+
+        this._script.loadScripts('app-profile',
+            ['//www.amcharts.com/lib/3/plugins/tools/polarScatter/polarScatter.min.js',
+                '//www.amcharts.com/lib/3/plugins/export/export.min.js',
+                'assets/app/js/services.js']);
+
+    }
+
+    onSelectionCurrency(value:string){
+
+        if(value=="Digital"){
+            this.currencyDDLHidden=true;
+        }else{
+            this.currencyDDLHidden=false;
+        }
     }
 
     changingRightTabStatus(value:string){
